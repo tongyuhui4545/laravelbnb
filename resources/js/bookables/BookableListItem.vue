7d0b506e6e570dc755de49b6a74b2692e2999d65
SAM <mel189good@gmail.com>
2020-10-22 13:53:27 +1100

    The BookableFactory is added

---
+++ resources/js/bookables/BookableListItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 calss="card-title">
+        <h1>{{ itemTitle }}</h1>
+      </h5>
+      <h5 calss="card-content">
+        <p>{{ itemContent }}</p>
+      </h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { itemTitle: String, itemContent: String, price: Number },
+};
+</script>
---
+++ resources/js/routes.js
@@ -0,0 +1,23 @@
+import VueRouter from "vue-router";
+import Bookables from "./bookables/Bookables";
+import Example2 from "./components/Example2.vue";
+
+const routes = [
+    {
+        path: "/",
+        component: Bookables,
+        name: "home"
+    },
+    {
+        path: "/second",
+        component: Example2,
+        name: "second"
+    }
+];
+
+const router = new VueRouter({
+    routes,
+    mode: "history",
+});
+
+export default router;
